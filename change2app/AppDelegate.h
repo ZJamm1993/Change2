35eacd133b1dfb20e0c82893b67e9f4f7b569f69
jam <jam@jamdemacbook-pro.local>
2017-04-11 09:29:18 +0800

    Initial Commit

---
+++ change2app/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  change2app
+//
+//  Created by jam on 17/4/11.
+//  Copyright © 2017年 jam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ change2app/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  change2app
+//
+//  Created by jam on 17/4/11.
+//  Copyright © 2017年 jam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
